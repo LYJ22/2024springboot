4f2c766240ad2b32c3b39822bb9b13b8f0710395
LYJ22 <pippi478@gmail.com>
2024-10-04 15:36:56 +0900

    make CRUD with mysql

--- Ex05_SimpleBbs/src/main/java/com/sample/spring/dao/ISimpleBbsDao.java
+++ Ex05_SimpleBbs/src/main/java/com/sample/spring/dao/ISimpleBbsDao.java
@@ -6,4 +6,8 @@ import com.sample.spring.dto.SimpleBbsDto;
 
 public interface ISimpleBbsDao {
 	public List<SimpleBbsDto> listDao();
+	public Integer countDao();
+	public SimpleBbsDto viewDao(String id);
+	public int writeDao(String writer, String title, String content);
+	public int delete(String id);
 }
--- Ex05_SimpleBbs/src/main/java/com/sample/spring/dao/SimpleBbsDao.java
+++ Ex05_SimpleBbs/src/main/java/com/sample/spring/dao/SimpleBbsDao.java
@@ -18,12 +18,45 @@ public class SimpleBbsDao implements ISimpleBbsDao{
 	@Override
 	public List<SimpleBbsDto> listDao() {
 		System.out.println("listDao()");
-		String query = "select * from simple_bbs order by id desc";
+		String query = "select * from simple_dbs order by id desc";
 		List<SimpleBbsDto> dtos = template.query(query,
 				new BeanPropertyRowMapper<>(SimpleBbsDto.class)
 				);
-		
 		return dtos;
 	}
+	
+	@Override
+	public Integer countDao() {
+		System.out.println("countDao()");
+		String query = "select count(*) from simple_dbs";
+		Integer count = template.queryForObject(query, Integer.class);
+		
+		return count;
+	}
 
+	@Override
+	public SimpleBbsDto viewDao(String id) {
+		System.out.println("viewDao()");
+		String query = "select * from simple_dbs where id = " + id;
+		SimpleBbsDto dto = template.queryForObject(query, new BeanPropertyRowMapper<>(SimpleBbsDto.class));
+		return dto;
+	}
+
+	@Override
+	public int writeDao(String writer, String title, String content) {
+		System.out.println("writeDao()");
+		String query = "INSERT INTO simple_dbs (writer, title, content) VALUES (?,?,?)";
+		int dtoup = template.update(query, writer, title, content);
+		return dtoup;
+	}
+
+	@Override
+	public int delete(String id) {
+		System.out.println("delete()");
+		String query = "DELETE FROM simple_dbs WHERE id = ?";
+		int dtodel = template.update(query, Integer.parseInt(id));
+		return dtodel;
+	}
+
+	
 }
