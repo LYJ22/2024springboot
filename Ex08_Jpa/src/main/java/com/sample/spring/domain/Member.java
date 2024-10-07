027745c58ffd10329ff80b8583da33e7cb30fab5
LYJ22 <pippi478@gmail.com>
2024-10-07 17:39:46 +0900

    service단 연습과 jpa

---
+++ Ex08_Jpa/src/main/java/com/sample/spring/domain/Member.java
@@ -0,0 +1,34 @@
+package com.sample.spring.domain;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+//요즘은 Setter 대신 Change를 사용하는 경우가 많다.
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "test")
+public class Member {
+	//primary key 잡는 방법. 변수명이 id라서 id가 아님. memberId여도 id 태그.
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length=100)
+	private String name;
+	@Column(name = "uemail")
+	private String email;
+	private LocalDate createDate;
+}
