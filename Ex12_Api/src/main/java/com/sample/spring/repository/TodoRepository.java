1e46644739c29e75415fe6da86d44be87a5eee8e
LYJ22 <pippi478@gmail.com>
2024-10-15 17:41:52 +0900

    jpa와 pagination, file upload/download

---
+++ Ex12_Api/src/main/java/com/sample/spring/repository/TodoRepository.java
@@ -0,0 +1,10 @@
+package com.sample.spring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sample.spring.model.TodoEntity;
+import com.sample.spring.repository.search.TodoSearch;
+
+public interface TodoRepository extends JpaRepository<TodoEntity, Long>, TodoSearch{
+
+}
